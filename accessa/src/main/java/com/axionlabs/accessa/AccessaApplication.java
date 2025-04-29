bbb38dd0a5dc60a27c089365b13d2fa18f067f0d
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-04-29 15:10:22 +0530

    chore(accessa): initialized repository

---
+++ accessa/src/main/java/com/axionlabs/accessa/AccessaApplication.java
@@ -0,0 +1,13 @@
+package com.axionlabs.accessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessaApplication.class, args);
+	}
+
+}
---
+++ accessa/src/test/java/com/axionlabs/accessa/AccessaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axionlabs.accessa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
