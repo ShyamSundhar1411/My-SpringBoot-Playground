70c1742d5bc5b6427d7701a81242dadbd51d455d
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-04-29 15:46:56 +0530

    feat(accessa): added initial configs

--- accessa/src/main/java/com/axionlabs/accessa/AccessaApplication.java
+++ accessa/src/main/java/com/axionlabs/accessa/AccessaApplication.java
@@ -1,9 +1,29 @@
 package com.axionlabs.accessa;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accessa API Documentation",
+				description = "Comprehensive documentation for the Accessa authentication APIs, supporting secure user registration, login, and role-based access using JWT.",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Shyam Sundhar",
+						email = "clashwithchiefrpjyt@gmail.com",
+						url = "https://shyamsundhar.vercel.app"
+				),
+				license = @License(
+						name = "MIT License",
+						url = "https://opensource.org/licenses/MIT"
+				)
+		)
+)
 public class AccessaApplication {
 
 	public static void main(String[] args) {
