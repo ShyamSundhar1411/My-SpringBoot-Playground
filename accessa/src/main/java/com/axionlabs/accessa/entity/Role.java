e803b154a3794f24f7e6f9560ee943a9cc7d60bd
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-04-29 22:37:11 +0530

    feat(accessa): added user entity

---
+++ accessa/src/main/java/com/axionlabs/accessa/entity/Role.java
@@ -0,0 +1,6 @@
+package com.axionlabs.accessa.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ accessa/src/main/java/com/axionlabs/accessa/entity/User.java
@@ -0,0 +1,48 @@
+package com.axionlabs.accessa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "user")
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+    private String userName;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    /**
+     * Returns the authorities granted to the user. Cannot return <code>null</code>.
+     *
+     * @return the authorities, sorted by natural key (never <code>null</code>)
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    /**
+     * Returns the username used to authenticate the user. Cannot return
+     * <code>null</code>.
+     *
+     * @return the username (never <code>null</code>)
+     */
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+}
