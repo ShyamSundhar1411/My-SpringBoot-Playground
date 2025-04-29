2bb80470bfd176d4cfc8c6c575752e6f972eed42
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-04-29 22:56:12 +0530

    feat(accessa): added jwt dependency

---
+++ accessa/src/main/java/com/axionlabs/accessa/service/JWTService.java
@@ -0,0 +1,11 @@
+package com.axionlabs.accessa.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    public String extractUserName(String jwtToken) {
+        return null;
+    }
+}
