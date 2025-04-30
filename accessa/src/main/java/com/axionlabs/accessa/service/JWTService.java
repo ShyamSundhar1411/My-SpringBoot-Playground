474cc1ae98d1c011240fcee89f68f925e4d89e76
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-04-30 14:37:13 +0530

    feat: added token generator

--- accessa/src/main/java/com/axionlabs/accessa/service/JWTService.java
+++ accessa/src/main/java/com/axionlabs/accessa/service/JWTService.java
@@ -2,11 +2,16 @@ package com.axionlabs.accessa.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -18,6 +23,24 @@ public class JWTService {
     public <T> T extractClaim(String jwtToken, Function<Claims,T> claimsResolver){
         final Claims claims = extractAllClaims(jwtToken);
         return claimsResolver.apply(claims);
+    }
+    public String generateJwtToken(UserDetails userDetails){
+        return generateJwtToken(new HashMap<>(),userDetails);
+    }
+    public String generateJwtToken(
+            Map<String,Object> extraClaims,
+            UserDetails userDetails
+    ){
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000*60*48))
+                .signWith(getSignInKey())
+                .compact();
+
+
     }
     private Claims extractAllClaims(String jwtToken){
         return Jwts.parser()
