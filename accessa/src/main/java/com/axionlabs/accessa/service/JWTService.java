83a0445c22472f1151548aec8821a5178a64a489
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-04-30 14:29:09 +0530

    feat(accessa): added claim extracting funcationalities

--- accessa/src/main/java/com/axionlabs/accessa/service/JWTService.java
+++ accessa/src/main/java/com/axionlabs/accessa/service/JWTService.java
@@ -1,11 +1,34 @@
 package com.axionlabs.accessa.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
 
 @Service
 public class JWTService {
-
+    private static final String SECRET_KEY = "1FC5142271728779143A39ACCB651";
     public String extractUserName(String jwtToken) {
-        return null;
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+    public <T> T extractClaim(String jwtToken, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+    private Claims extractAllClaims(String jwtToken){
+        return Jwts.parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload();
+    }
+
+    private SecretKey getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
