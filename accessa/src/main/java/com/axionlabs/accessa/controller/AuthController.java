07f7a8fedde55f9420db40368c319070509f2042
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-05-02 16:01:47 +0530

    feat(accessa): added api documentation

--- accessa/src/main/java/com/axionlabs/accessa/controller/AuthController.java
+++ accessa/src/main/java/com/axionlabs/accessa/controller/AuthController.java
@@ -1,10 +1,16 @@
 package com.axionlabs.accessa.controller;
 
+import com.axionlabs.accessa.dto.ErrorResponseDto;
 import com.axionlabs.accessa.dto.user.TokenizedUserDto;
 import com.axionlabs.accessa.dto.user.request.LoginRequestDto;
 import com.axionlabs.accessa.dto.user.request.RegisterRequestDto;
 import com.axionlabs.accessa.dto.user.response.UserResponseDto;
 import com.axionlabs.accessa.service.impl.IAuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -28,6 +34,27 @@ public class AuthController {
         this.iAuthService = iAuthService;
     }
 
+    @Operation(
+            summary = "Register a new user",
+            description = "Registers a new user into the system with the provided details. Returns user information and a tokenized response upon successful registration."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User registered successfully.",
+                    content = @Content(schema = @Schema(implementation = UserResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid input data provided.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<UserResponseDto> register(
             @Valid @RequestBody RegisterRequestDto request
@@ -46,6 +73,32 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @Operation(
+            summary = "Login a user",
+            description = "Logs in a user using provided credentials and returns a tokenized response if successful."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User logged in successfully.",
+                    content = @Content(schema = @Schema(implementation = UserResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid input data provided.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized access (invalid credentials).",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal Server Error.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))
+            )
+    })
     public ResponseEntity<UserResponseDto> login(@Valid @RequestBody LoginRequestDto request){
         TokenizedUserDto userDetails = iAuthService.loginUser(request);
         return ResponseEntity.status(
