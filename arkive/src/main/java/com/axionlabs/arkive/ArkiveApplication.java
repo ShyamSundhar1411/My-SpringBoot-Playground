eaa02c5db4a4459b111c626f5cf0e6c61a07aa9f
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-05-13 13:49:37 +0530

    chore(arkive): added intial api configuration

--- arkive/src/main/java/com/axionlabs/arkive/ArkiveApplication.java
+++ arkive/src/main/java/com/axionlabs/arkive/ArkiveApplication.java
@@ -1,9 +1,30 @@
 package com.axionlabs.arkive;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Arkive API Documentation",
+				description = "Comprehensive documentation for the Arkive APIs, supporting secure user login and file uploads to Amazon S3.",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Shyam Sundhar",
+						email = "clashwithchiefrpjyt@gmail.com",
+						url = "https://shyamsundhar.vercel.app"
+				),
+				license = @License(
+						name = "MIT License",
+						url = "https://opensource.org/licenses/MIT"
+				)
+		)
+)
 public class ArkiveApplication {
 
 	public static void main(String[] args) {
