528a8286e381134ba1bc88b9b4e5dde5efce6d2d
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-05-18 20:55:09 +0530

    feat(arkive): added environment initializer in application context initializer

--- arkive/src/main/java/com/axionlabs/arkive/config/EnvironmentConfiguration.java
+++ arkive/src/main/java/com/axionlabs/arkive/config/EnvironmentConfiguration.java
@@ -4,6 +4,10 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.MapPropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnvironmentConfiguration implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
@@ -15,5 +19,8 @@ public class EnvironmentConfiguration implements ApplicationContextInitializer<C
     @Override
     public void initialize(@NotNull ConfigurableApplicationContext applicationContext) {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        Map<String,Object> envMap = new HashMap<>();
+        dotenv.entries().forEach(entry -> envMap.put(entry.getKey(),entry.getValue()));
+        applicationContext.getEnvironment().getPropertySources().addFirst(new MapPropertySource("dotenv",envMap));
     }
 }
