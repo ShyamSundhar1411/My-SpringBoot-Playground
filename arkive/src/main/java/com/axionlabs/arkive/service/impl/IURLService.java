0530d30be9cce90191392620637106dbdf0d47fd
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-05-21 16:14:04 +0530

    fix(arkive): fixed bean injection for aws config properties

--- arkive/src/main/java/com/axionlabs/arkive/service/impl/IURLService.java
+++ arkive/src/main/java/com/axionlabs/arkive/service/impl/IURLService.java
@@ -3,6 +3,7 @@ package com.axionlabs.arkive.service.impl;
 import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.axionlabs.arkive.config.properties.AWSConfigProperties;
 import com.axionlabs.arkive.service.URLService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,18 +14,18 @@ import java.util.Date;
 @Service
 public class IURLService implements URLService {
     private final AmazonS3 amazonS3;
-    private final String s3BucketName;
+    private final AWSConfigProperties awsConfigProperties;
 
     @Autowired
-    public IURLService(AmazonS3 amazonS3, String s3BucketName){
+    public IURLService(AmazonS3 amazonS3, AWSConfigProperties awsConfigProperties){
         this.amazonS3 = amazonS3;
-        this.s3BucketName = s3BucketName;
+        this.awsConfigProperties = awsConfigProperties;
     }
 
     @Override
     public String generatePreSignedUploadUrl(String fileName, int expirationMinutes) {
         Date expiration = new Date(System.currentTimeMillis() + (long) expirationMinutes * 60 * 1000);
-        GeneratePresignedUrlRequest presignedUrlRequest = new GeneratePresignedUrlRequest(s3BucketName, fileName)
+        GeneratePresignedUrlRequest presignedUrlRequest = new GeneratePresignedUrlRequest(awsConfigProperties.getBucketName(), fileName)
                 .withExpiration(expiration)
                 .withMethod(HttpMethod.PUT);
         URL url = amazonS3.generatePresignedUrl(presignedUrlRequest);
@@ -34,7 +35,7 @@ public class IURLService implements URLService {
     @Override
     public String generatePreSignedAccessUrl(String fileName, int expirationMinutes) {
         Date expiration = new Date(System.currentTimeMillis() + (long) expirationMinutes * 60 * 1000);
-        GeneratePresignedUrlRequest presignedUrlRequest = new GeneratePresignedUrlRequest(s3BucketName, fileName)
+        GeneratePresignedUrlRequest presignedUrlRequest = new GeneratePresignedUrlRequest(awsConfigProperties.getBucketName(), fileName)
                 .withMethod(HttpMethod.GET)
                 .withExpiration(expiration);
         URL url = amazonS3.generatePresignedUrl(presignedUrlRequest);
