e640bf8166105479e21ccaea76828dab1ab853b8
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-05-21 12:48:26 +0530

    feat(arkive): added jwt service functionalities

--- arkive/src/main/java/com/axionlabs/arkive/service/impl/IJWTService.java
+++ arkive/src/main/java/com/axionlabs/arkive/service/impl/IJWTService.java
@@ -5,42 +5,66 @@ import com.axionlabs.arkive.dto.token.TokenDto;
 import com.axionlabs.arkive.dto.token.request.TokenRequestDto;
 import com.axionlabs.arkive.service.JWTService;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class IJWTService implements JWTService {
     private final JWTConfigProperties jwtConfigProperties;
+    private final IUserService iUserService;
 
     @Autowired
-    public IJWTService(JWTConfigProperties jwtConfigProperties){
+    public IJWTService(JWTConfigProperties jwtConfigProperties, IUserService iUserService){
         this.jwtConfigProperties = jwtConfigProperties;
+        this.iUserService = iUserService;
     }
 
     @Override
     public String extractUserName(String jwtToken, boolean isAccess) {
-        return "";
+        return extractClaim(jwtToken,Claims::getSubject, isAccess);
     }
 
     @Override
     public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver, boolean isAccess) {
-        return null;
+        final Claims claims = extractAllClaims(jwtToken,isAccess);
+        return claimsResolver.apply(claims);
     }
 
     @Override
     public TokenDto generateJwtToken(UserDetails userDetails) {
-        return null;
+        TokenDto tokens = new TokenDto();
+        String accessToken = generateAccessToken(new HashMap<>(),userDetails);
+        String refreshToken = generateRefreshToken(new HashMap<>(),userDetails);
+        tokens.setAccessToken(accessToken);
+        tokens.setRefreshToken(refreshToken);
+        return tokens;
     }
 
     @Override
     public boolean isTokenValid(String token, UserDetails userDetails, boolean isAccess) {
-        return false;
+        final String username = extractUserName(token,isAccess);
+        return (username.equals(userDetails.getUsername()) && isTokenExpired(token,isAccess));
     }
 
+
+    private boolean isTokenExpired(String token, boolean isAccess){
+        return !extractExpiration(token, isAccess).before(new Date());
+    }
+
+    private Date extractExpiration(String token, boolean isAccess){
+        return extractClaim(token, Claims::getExpiration, isAccess);
+    }
     @Override
     public TokenDto generateJwtTokenFromRefreshToken(TokenRequestDto tokenRequest) {
         return null;
@@ -54,5 +78,21 @@ public class IJWTService implements JWTService {
     @Override
     public String generateRefreshToken(Map<String, Object> extraClaims, UserDetails userDetails) {
         return "";
+    }
+    private Claims extractAllClaims(String jwtToken,boolean isAccess){
+        return Jwts.parser()
+                .verifyWith(isAccess?getAccessSignInKey():getRefreshSignInKey())
+                .build()
+                .parseSignedClaims(jwtToken)
+                .getPayload();
+    }
+
+    private SecretKey getRefreshSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(jwtConfigProperties.getRefreshSecretKey());
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    private SecretKey getAccessSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(jwtConfigProperties.getAccessSecretKey());
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
