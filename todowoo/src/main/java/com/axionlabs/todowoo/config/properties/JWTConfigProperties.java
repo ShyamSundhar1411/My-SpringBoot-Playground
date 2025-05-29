1f083fb0059b08ae972a680b9a8686918a5be5eb
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-05-29 23:02:17 +0530

    feat(todowoo): added jwtconfiguration

---
+++ todowoo/src/main/java/com/axionlabs/todowoo/config/properties/JWTConfigProperties.java
@@ -0,0 +1,16 @@
+package com.axionlabs.todowoo.config.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "spring.token")
+@Getter @Setter
+public class JWTConfigProperties {
+    private String accessSecretKey;
+    private String refreshSecretKey;
+    private Long accessSecretKeyExpiration;
+    private Long refreshSecretKeyExpiration;
+}
