eb9fbab269e23e1e3d7e915f28387c1a54fdc6b3
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2025-05-31 14:55:13 +0530

    feat(todowoo): added mappers

--- todowoo/src/main/java/com/axionlabs/todowoo/mapper/ToDoMapper.java
+++ todowoo/src/main/java/com/axionlabs/todowoo/mapper/ToDoMapper.java
@@ -1,4 +1,11 @@
 package com.axionlabs.todowoo.mapper;
 
+import com.axionlabs.todowoo.dto.todo.ToDoDto;
+import com.axionlabs.todowoo.entity.ToDo;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
 public interface ToDoMapper {
+    ToDo fromToDoDto(ToDoDto toDoDto);
+    ToDoDto toToDoDto(ToDo toDo);
 }
--- todowoo/src/main/java/com/axionlabs/todowoo/mapper/UserMapper.java
+++ todowoo/src/main/java/com/axionlabs/todowoo/mapper/UserMapper.java
@@ -1,9 +1,11 @@
 package com.axionlabs.todowoo.mapper;
 
+import com.axionlabs.todowoo.dto.user.UserDto;
 import com.axionlabs.todowoo.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-
+    User fromUserDto(UserDto userDto);
+    UserDto toUserDto(User user);
 }
